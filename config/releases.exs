4365c7fa9da99f86970c3f002a86d33883231251
Josh Adams <josh@dbadbadba.com>
2021-09-05 10:29:51 -0500

    Set up sentry

--- config/releases.exs
+++ config/releases.exs
@@ -64,3 +64,13 @@ config :pleroma, Pleroma.PromEx,
     folder_name: "Pleroma - PromEx",
     annotate_app_lifecycle: true
   ]
+
+config :sentry,
+  dsn: System.fetch_env!("SENTRY_DSN"),
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!(),
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
--- lib/pleroma/web/endpoint.ex
+++ lib/pleroma/web/endpoint.ex
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Pleroma.Web.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :pleroma
 
   require Pleroma.Constants
